6ed1db3305b4f60f47e15ec9b2c2540a1147efeb
George Tsatsis <george@ratelimited.me>
2022-01-31 13:09:06 +0200

    impl: error responses are now formatted correctly

--- errors.go
+++ errors.go
@@ -3,13 +3,22 @@ package main
 import "github.com/gofiber/fiber/v2"
 
 var MissingCredentialsResponse = fiber.Map{
-	"error": "Missing Credentials!",
+	"error": fiber.Map{
+		"message":     "Missing Credentials!",
+		"status_code": 401,
+	},
 }
 
 var InvalidCredentialsResponse = fiber.Map{
-	"error": "Invalid Credentials!",
+	"error": fiber.Map{
+		"message":     "Invalid Credentials!",
+		"status_code": 401,
+	},
 }
 
 var TwoFactorAuthenticationRequiredResponse = fiber.Map{
-	"error": "Two Factor Authentication Required!",
+	"error": fiber.Map{
+		"message":     "Two Factor Authentication Required!",
+		"status_code": 401,
+	},
 }
