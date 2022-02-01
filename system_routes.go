ce4b14c8653b98068a314e2be91dd232ebbb7bf2
George Tsatsis <george@ratelimited.me>
2022-02-01 02:39:38 +0200

    impl: logout route

--- system_routes.go
+++ system_routes.go
@@ -1,10 +1,14 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"time"
+)
 
 func systemRoutes(router *fiber.App) {
 	router.Get("/config", GetConfig)
 	router.Get("/health", GetHealth)
+	router.Put("/logout", Logout)
 }
 
 func GetHealth(c *fiber.Ctx) error {
@@ -21,4 +25,20 @@ func GetConfig(c *fiber.Ctx) error {
 		Value: ApiConfiguration.ApiKey.Get(),
 	})
 	return c.JSON(NewApiConfigResponse(&ApiConfiguration))
+}
+
+func Logout(c *fiber.Ctx) error {
+	c.Cookie(&fiber.Cookie{
+		Name:    "auth",
+		Value:   "",
+		Expires: time.Now().Add(time.Hour * -1),
+	})
+	c.Cookie(&fiber.Cookie{
+		Name:    "twoFactorAuth",
+		Value:   "",
+		Expires: time.Now().Add(time.Hour * -1),
+	})
+	return c.Status(200).JSON(fiber.Map{
+		"status": "ok",
+	})
 }
