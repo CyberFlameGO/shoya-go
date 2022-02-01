0182e2117c68bec6e615fbef1abd44b0c16b6444
George Tsatsis <george@ratelimited.me>
2022-02-01 05:19:31 +0200

    impl: /auth/exists

--- auth_routes.go
+++ auth_routes.go
@@ -1,11 +1,15 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
 
 func authRoutes(router *fiber.App) {
 	router.Get("/auth", ApiKeyMiddleware, AuthMiddleware, getAuth)
+	router.Get("/auth/exists", ApiKeyMiddleware, getExists)
 	router.Get("/auth/user", ApiKeyMiddleware, DoLoginMiddleware, AuthMiddleware, getSelf)
-
+	router.Get("/auth/user/notifications", ApiKeyMiddleware, AuthMiddleware, getNotifications)
 }
 
 func getAuth(c *fiber.Ctx) error {
@@ -15,8 +19,34 @@ func getAuth(c *fiber.Ctx) error {
 	})
 }
 
+func getExists(c *fiber.Ctx) error {
+	var u User
+	var exists = true
+
+	tx := DB.Where("username = ?", c.Query("username")).
+		Or("display_name = ?", c.Query("displayName")).
+		Or("email = ?", c.Query("email")).Select("id").First(&u)
+
+	if tx.Error != nil {
+		if tx.Error == gorm.ErrRecordNotFound {
+			exists = false
+		}
+	}
+	return c.Status(200).JSON(fiber.Map{
+		"userExists": exists,
+	})
+}
+
 func getSelf(c *fiber.Ctx) error {
 	u := c.Locals("user").(*User)
 
 	return c.Status(200).JSON(u)
+}
+
+func getNotifications(c *fiber.Ctx) error {
+	u := c.Locals("user").(*User)
+
+	return c.Status(200).JSON(fiber.Map{
+		"user": u,
+	}) // TODO: implement
 }
