06c2b3debe6bfae4ddc747fab2ef129ea17022e5
George Tsatsis <george@ratelimited.me>
2022-05-19 19:11:12 +0300

    chore: mark use of math/rand as a non-issue (use is not cryptographic)

--- api/middleware.go
+++ api/middleware.go
@@ -14,7 +14,7 @@ import (
 )
 
 func AddXPoweredByHeader(c *fiber.Ctx) error {
-	c.Set("X-Powered-By", models.XPoweredByHeaders[rand.Intn(len(models.XPoweredByHeaders))])
+	c.Set("X-Powered-By", models.XPoweredByHeaders[rand.Intn(len(models.XPoweredByHeaders))]) // #nosec skipcq
 	return c.Next()
 }
 
