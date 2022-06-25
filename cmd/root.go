b87d2cf7773a37f6258474d0754089702dd73371
George Tsatsis <george@ratelimited.me>
2022-06-25 22:18:41 +0300

    feat: config file param, added goland build configs

--- cmd/root.go
+++ cmd/root.go
@@ -3,13 +3,28 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"gitlab.com/george/shoya-go/config"
+	"log"
 	"os"
 )
+
+var ConfigLocation string
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&ConfigLocation, "config", "c", "config.json", "The location of the config file")
+}
 
 var rootCmd = &cobra.Command{
 	Use:   "shoya",
 	Short: "An API emulator for VRChat",
 	Long:  `Shoya is an API emulator ("private server") for the popular VR social game, VRChat.`,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		ConfigLocation = cmd.Flag("config").Value.String()
+		err := config.LoadConfig(ConfigLocation)
+		if err != nil {
+			log.Fatalf("%+v", err)
+		}
+	},
 }
 
 func Execute() {
